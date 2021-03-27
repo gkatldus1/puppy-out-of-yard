bf25b1278e84afd3363c17890d6a9cc666309deb
Your Name <gkatldus1@gmail.com>
2021-03-27 15:34:42 +0900

    조금만 수정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //수정 조금만 해봤습니다.
     }
 }
