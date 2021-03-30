c1c770140dfa77f45e2d8a93f2c08d05b25c47ac
Your Name <gkatldus1@gmail.com>
2021-03-30 23:04:27 +0900

    패키지만들어조

--- app/src/main/java/com/capstone/puppy/SelectPuppyActivity.java
+++ app/src/main/java/com/capstone/puppy/SelectPuppyActivity.java
@@ -3,12 +3,24 @@ package com.capstone.puppy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SelectPuppyActivity extends AppCompatActivity {
+
+    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"} ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_puppy);
+        //리스트뷰에 출력을 도와줄 어댑터 생성
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU) ;
+
+        ListView listview = (ListView) findViewById(R.id.listview1) ;
+        listview.setAdapter(adapter) ;
+
+
+
     }
 }
