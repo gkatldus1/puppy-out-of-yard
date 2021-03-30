f44aef1dafa8bdde57ea0ae05342650f2623de17
yjoh <856251@naver.com>
2021-03-30 23:04:40 +0900

    액티비티 이동 구현

--- app/src/main/java/com/capstone/puppy/MainActivity.java
+++ app/src/main/java/com/capstone/puppy/MainActivity.java
@@ -2,14 +2,31 @@ package com.capstone.puppy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btn_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //수정 조금만 해봤습니다.
+
+        btn_menu = findViewById(R.id.btn_menu);
+        btn_menu.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_menu:
+                Intent intent = new Intent(this, SelectPuppyActivity.class);
+                startActivity(intent);
+                break;
+
+        }
     }
 }
