9de234a2028900b55c5b5a03b506f5ea002c38d7
dasher3802 <dasher3802@gmail.com>
2021-04-04 16:17:50 +0900

    지도api적용

--- app/src/main/java/com/capstone/puppy/MainActivity.java
+++ app/src/main/java/com/capstone/puppy/MainActivity.java
@@ -2,16 +2,29 @@ package com.capstone.puppy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btn_menu;
-    MapView map;
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapReverseGeoCoder;
+import net.daum.mf.map.api.MapView;
 
+import java.security.MessageDigest;
+import java.security.Signature;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, net.daum.mf.map.api.MapView.CurrentLocationEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener, MapViewEventListener {
+    Button btn_menu;
+    private MapView mMapView;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +33,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_menu = findViewById(R.id.btn_menu);
         btn_menu.setOnClickListener(this);
 
-        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(map);
+        mMapView = (MapView) findViewById(R.id.map_view);
+        mMapView.setDaumMapApiKey(MapApiConst.DAUM_MAPS_ANDROID_APP_API_KEY);
+        mMapView.setMapViewEventListener(this);
+
+        // getAppKeyHash();
     }
 
     @Override
@@ -33,5 +49,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
         }
+    }
+
+    private void getAppKeyHash() {
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
+        }
+    }
+
+    @Override
+    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+
+    }
+
+    @Override
+    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdate(net.daum.mf.map.api.MapView mapView, MapPoint mapPoint, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(net.daum.mf.map.api.MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(net.daum.mf.map.api.MapView mapView) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(net.daum.mf.map.api.MapView mapView) {
+
+    }
+
+    @Override
+    public void onLoadMapView() {
+
     }
 }
